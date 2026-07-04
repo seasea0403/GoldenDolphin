1f4f20ef472cd4918ae2def83342d25ec4e69e23
seasea0403​​ <3040745254@qq.com>
2026-07-04 22:22:18 +0800

    合并提交

---
+++ Assets/LingBoCanteen/UI_Scripts/SettingButtonDOTween.cs
@@ -0,0 +1,58 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SettingButtonDOTween : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public Image normalImage;      
+    public Image hoverImage;       
+    public float heightAdd = 15f; 
+    public float dur = 0.2f;      
+
+    private RectTransform rt;
+    private float origH;           
+    private Vector2 origSize;     
+
+    void Awake()
+    {
+        rt = GetComponent<RectTransform>();
+        origH = rt.sizeDelta.y;
+        origSize = rt.sizeDelta;
+
+        if (normalImage != null) normalImage.gameObject.SetActive(true);
+        if (hoverImage != null) hoverImage.gameObject.SetActive(false);
+
+        if (normalImage != null) normalImage.color = Color.white;
+        if (hoverImage != null) hoverImage.color = Color.white;
+    }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+        ActivateHover(true);
+    }
+
+    public void OnPointerExit(PointerEventData data)
+    {
+        ActivateHover(false);
+    }
+
+    public void OnPointerDown(PointerEventData data) { ActivateHover(true); }
+    public void OnPointerUp(PointerEventData data) { ActivateHover(false); }
+
+    void ActivateHover(bool isOn)
+    {
+        // 1. 处理物体显隐
+        if (hoverImage != null) hoverImage.gameObject.SetActive(isOn);
+        if (normalImage != null) normalImage.gameObject.SetActive(!isOn);
+
+        // 2. 处理尺寸动画
+        float targetH = isOn ? origH + heightAdd : origH;
+
+        // 如果高度有变化才执行动画
+        if (Mathf.Abs(rt.sizeDelta.y - targetH) > 0.1f)
+        {
+            rt.DOSizeDelta(new Vector2(rt.sizeDelta.x, targetH), dur).SetEase(Ease.OutCubic);
+        }
+    }
+}
